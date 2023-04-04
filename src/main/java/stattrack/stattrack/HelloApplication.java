182d7d7da56ed5845ce6c11f90b2265ff0f21b9d
sarka99 <119513443+sarka99@users.noreply.github.com>
2023-04-04 11:38:21 +0200

    test
    testing pushing to my own branch

--- src/main/java/stattrack/stattrack/HelloApplication.java
+++ src/main/java/stattrack/stattrack/HelloApplication.java
@@ -19,5 +19,7 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+
+        System.out.println("hello,world its sargon pushing");
     }
 }
