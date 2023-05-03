41a885d1834e29808693e41593b9bb00ff7c21a0
alexhasan99 <mohammad.hasan19999@gmail.com>
2023-05-03 22:45:20 +0200

    hh

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module stattrack.stattrack {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires scb.java.client;
+    requires java.desktop;
 
     opens stattrack.stattrack to javafx.fxml;
     exports stattrack.stattrack;
---
+++ src/main/java/stattrack/stattrack/Test.java
@@ -0,0 +1,10 @@
+package stattrack.stattrack;
+
+import com.github.dannil.scbjavaclient.client.SCBClient;
+
+public class Test {
+
+    public static void main(String[] args) {
+        SCBClient client = new SCBClient();
+    }
+}
